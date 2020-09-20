4ef6472c787731a0bb1e1a480b57aac9f93085b4
Natan <nattantavares.s15@gmail.com>
2020-09-20 10:03:44 -0300

    Organizing the components within the calculator

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,32 +2,13 @@ import React from "react";
 
 import "./styles.css";
 
-export default (props: any) => {
+interface ButtonProp {
+  label: String
+}
+
+export default (props: ButtonProp) => {
+  const { label } = props;
   return (
-    <div className="keyboard">
-      <button className="button">AC</button>
-      <button className="button">±</button>
-      <button className="button">%</button>
-      <button className="button">÷</button>
-
-      <button className="button">7</button>
-      <button className="button">8</button>
-      <button className="button">9</button>
-      <button className="button">X</button>
-
-      <button className="button">4</button>
-      <button className="button">5</button>
-      <button className="button">6</button>
-      <button className="button">-</button>
-
-      <button className="button">1</button>
-      <button className="button">2</button>
-      <button className="button">3</button>
-      <button className="button">+</button>
-
-      <button className="button">0</button>
-      <button className="button">.</button>
-      <button className="button">=</button>
-    </div>
+    <button className="button">{label}</button>
   );
 }
--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Display from "../Display";
 import Button from "../Button";
 
 import "./styles.css";
@@ -8,8 +9,32 @@ export default class Calculator extends Component {
   render() {
     return (
       <div className="Calculator">
-        <p>I'm a calculator</p>
-        <Button />
+        <Display value={100} />
+        
+        {/** Keyboard */}
+        <Button label="AC" />
+        <Button label="±" />
+        <Button label="%" />
+        <Button label="÷" />
+
+        <Button label="7" />
+        <Button label="8" />
+        <Button label="9" />
+        <Button label="x" />
+
+        <Button label="4" />
+        <Button label="5" />
+        <Button label="6" />
+        <Button label="-" />
+
+        <Button label="1" />
+        <Button label="2" />
+        <Button label="3" />
+        <Button label="+" />
+
+        <Button label="0" />
+        <Button label="." />
+        <Button label="=" />
       </div>
     );
   }
